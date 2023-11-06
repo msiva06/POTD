7daf124128c589b9c0e83dbd14f4ee6230793114
msiva06 <m.sivasubramanian06@gmail.com>
2023-11-06 10:32:27 -0500

    Seat Reservation Manager added

---
+++ Problem_06.java
@@ -0,0 +1,20 @@
+// Seat Reservation Manager
+public class Problem_06 {
+    PriorityQueue<Integer> pq;
+    int marker;
+    public SeatManager(int n) {
+        pq = new PriorityQueue<Integer>();
+        marker = 1;
+    }
+    
+    public int reserve() {
+        if(!pq.isEmpty()){
+            return pq.poll();
+        }
+        return marker++;
+    }
+    
+    public void unreserve(int seatNumber) {
+        pq.offer(seatNumber);
+    }
+}
