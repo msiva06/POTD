de95e6b493d3eb6da5536a6c2279de738ad63399
msiva06 <m.sivasubramanian06@gmail.com>
2023-11-19 19:20:44 -0500

    Reduction operations to make array elements equal added

---
+++ Problem_19.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+// Reduction operations to make array elements equal
+public class Problem_19 {
+    public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0, up = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i-1])
+                up++;
+            ans += up;
+        }
+        return ans;
+        
+    }
+}
