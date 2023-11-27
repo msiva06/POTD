0b932597660d727f50847caf42d4b0b0f677705b
msiva06 <m.sivasubramanian06@gmail.com>
2023-11-26 22:10:48 -0500

    Largest submatrix with Rearrangements added

---
+++ Problem_26.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+// Largest submatrix with rearrangements
+public class Problem_26 {
+    public int largestSubmatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] prevRow = new int[cols];
+        int ans = 0;
+        for(int row = 0; row < rows; row++){
+            int[] curRow = new int[cols];
+            for(int col = 0; col < cols; col++){
+                if(matrix[row][col] == 1){
+                    curRow[col] = prevRow[col]+1;
+                }
+            }
+                int[] sortedRow = curRow.clone();
+                Arrays.sort(sortedRow);
+                for(int k = 0; k < cols; k++){
+                    ans = Math.max(ans,sortedRow[k] * (cols-k));
+                }
+            prevRow = curRow;
+        }
+        return ans;
+    }
+}
