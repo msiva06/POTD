fcd407e4aacd0837a0e4a9e80d941f5f1ac774d2
msiva06 <m.sivasubramanian06@gmail.com>
2023-12-10 10:16:10 -0500

    Transpose matrix added

---
+++ December/Problem_10.java
@@ -0,0 +1,16 @@
+package December;
+
+// Transpose Matrix
+public class Problem_10 {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] res = new int[m][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+}
