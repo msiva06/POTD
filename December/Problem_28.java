7e95f9e52bf9d24a994e6835b24a4edf0cbd2302
msiva06 <m.sivasubramanian06@gmail.com>
2023-12-27 11:03:11 -0500

    included comments

---
+++ December/Problem_28.java
@@ -0,0 +1,21 @@
+package December;
+
+// Min time to make rope colorful 
+public class Problem_28 {
+    public int minCost(String colors, int[] neededTime) {
+        int left = 0,  right = 0;
+        int totalTime = 0;
+        while(left < neededTime.length && right < neededTime.length){
+           int currTotal = 0, currMax = 0;
+           while(right < neededTime.length && colors.charAt(left) == colors.charAt(right)){
+               currTotal += neededTime[right];
+               currMax = Math.max(currMax,neededTime[right]);
+               right++;
+           }
+           int minTime = currTotal - currMax;
+           totalTime += minTime;
+           left = right;
+        }
+        return totalTime;
+    }
+}
