592fbfe5db5467c0698c74e81aaba0910b8e6335
msiva06 <m.sivasubramanian06@gmail.com>
2024-02-21 12:15:39 -0500

    Missing number

--- February/Problem_19.java
+++ February/Problem_19.java
@@ -5,20 +5,10 @@ import java.util.Arrays;
 // Missing number
 public class Problem_19 {
     public int missingNumber(int[] nums) {
-        int[] res = new int[nums.length];
-        Arrays.fill(res,-1);
+        int missing = nums.length;
         for(int i = 0; i < nums.length; i++){
-            int val = nums[i];
-            if(val < nums.length){
-                res[val] = val;
-            }
+            missing ^= i ^ nums[i];
         }
-
-        for(int i = 0; i < res.length; i++){
-            if(res[i] != i){
-                return i;
-            }
-        }
-        return res.length;
+        return missing;
     }
 }
