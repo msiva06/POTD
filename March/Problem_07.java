7b05fca67329ed8f6651b24c0b9161644a1df5ec
msiva06 <m.sivasubramanian06@gmail.com>
2024-03-07 09:11:35 -0500

    Middle of LinkedList

---
+++ March/Problem_07.java
@@ -0,0 +1,14 @@
+package March;
+
+// Middle of linked list
+public class Problem_07 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
