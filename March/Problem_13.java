5a65905d87a84ec09ad5d262eac1b56629bd1fc7
msiva06 <m.sivasubramanian06@gmail.com>
2024-03-14 05:24:20 -0400

    Corrected Typo

--- March/Problem_13.java
+++ March/Problem_13.java
@@ -3,7 +3,7 @@ package March;
 // Pivot Integer
 public class Problem_13 {
     public int pivotInteger(int n) {
-        nt left = 1, right = n;
+        int left = 1, right = n;
         int totalSum = (n * (n+1)) / 2;
         while(left < right){
             int mid = left + (right - left)/2;
