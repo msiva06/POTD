1f8cfb9944aab19b22410b3aebb7e395210992c2
msiva06 <m.sivasubramanian06@gmail.com>
2024-03-24 12:13:34 -0400

    Find Duplicates

---
+++ March/Problem_24.java
@@ -0,0 +1,21 @@
+package March;
+
+// Find Duplicates
+public class Problem_24 {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast)
+                break;
+        }
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
